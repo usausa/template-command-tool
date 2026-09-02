6cae1bb38375db5b7e246f07fa72a19bb3fb8e39
usausa <machi.pon@gmail.com>
2026-09-02 14:37:22 +0900

    Update

--- Template.CommandTool/Commands/DataCommand.cs
+++ Template.CommandTool/Commands/DataCommand.cs
@@ -3,6 +3,4 @@ namespace Template.CommandTool.Commands;
 using Smart.CommandLine.Hosting;
 
 [Command("data", "Data operation")]
-public sealed class DataCommand
-{
-}
+public sealed class DataCommand;
